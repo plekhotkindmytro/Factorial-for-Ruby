c11673452135e4e90d1890a8ecfc1cc2862622b2
Dmytro <plekhotkindmytro@gmail.com>
2016-01-23 19:04:42 +0200

    Fix bug when number equals to 0. 0! equals to 1

--- factorial.rb
+++ factorial.rb
@@ -2,21 +2,27 @@ puts "Enter a positive integer number(Minimum value is 1)"
 numberString = gets.chomp 
 number = numberString.to_i
 
-while number <= 0
-  puts "#{numberString} is not valid number. Please enter positive number: "
-  numberString = gets.chomp
-  number = numberString.to_i;
-end 
-
 def factorial(integer_number)
-  i = 1
-  fact = 1;
-  while i <= integer_number do
-    fact*=i
-    i+=1
+    i = 1
+    fact = 1;
+    while i <= integer_number do
+        fact*=i
+        i+=1
+    end
+    return fact
+end
+    
+if numberString == "0"
+  puts "Result: ";
+  puts "1"
+else 
+  while number <= 0
+    puts "#{numberString} is not valid number. Please enter positive number: "
+    numberString = gets.chomp
+    number = numberString.to_i;
   end
-  return fact
+  puts "Result: "
+  puts factorial(number)
 end
 
-puts "Result:"
-puts factorial(number)
+
